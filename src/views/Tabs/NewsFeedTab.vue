149aa22607bbe59056ce988c7b1b0d94e78ce4b9
badchita <47920377+badchita@users.noreply.github.com>
2021-09-03 20:32:02 +0800

    NewsFeed.vue
    -audio & video rooms skeleton

--- src/views/Tabs/NewsFeedTab.vue
+++ src/views/Tabs/NewsFeedTab.vue
@@ -19,14 +19,33 @@
 
         <ion-row class="button-row">
           <ion-col>
-            <ion-button fill="clear"><ion-icon name='videocam' color="danger"/>Live</ion-button>
+            <ion-button fill="clear">
+              <ion-icon name='videocam' color="danger" />Live</ion-button>
           </ion-col>
           <ion-col>
-            <ion-button fill="clear"><ion-icon name='images' color="success"/>Photo</ion-button>
+            <ion-button fill="clear">
+              <ion-icon name='images' color="success" />Photo</ion-button>
           </ion-col>
           <ion-col>
-            <ion-button fill="clear"><ion-icon name='videocam' color="tertiary"/>Room</ion-button>
+            <ion-button fill="clear">
+              <ion-icon name='videocam' color="tertiary" />Room</ion-button>
           </ion-col>
+        </ion-row>
+      </ion-grid>
+
+      <ion-grid class="audio-video-rooms">
+        <ion-row>
+          <ion-label>Audio & Video Rooms</ion-label>
+        </ion-row>
+        <ion-row>
+          <ion-slides :options="slideOption">
+            <ion-button>Create Room</ion-button>
+            <ion-slide v-for="(item,i) in 7" :key="i">
+              <ion-avatar>
+                <ion-img src="https://pickaface.net/gallery/avatar/unr_test_180821_0925_9k0pgs.png" />
+              </ion-avatar>
+            </ion-slide>
+          </ion-slides>
         </ion-row>
       </ion-grid>
     </ion-content>
@@ -45,9 +64,13 @@
     IonImg,
     IonButton,
     IonIcon,
+    IonLabel,
+    IonSlide,
+    IonSlides,
   } from '@ionic/vue';
 
   import MainHeader from '@/components/MainHeader.vue'
+import { onMounted } from '@vue/runtime-core';
 
   export default {
     name: 'NewsFeedTab',
@@ -62,8 +85,24 @@
       IonImg,
       IonButton,
       IonIcon,
+      IonLabel,
+      IonSlide,
+      IonSlides,
 
       MainHeader
+    },
+    setup() {
+      onMounted(() => {
+        slideOption
+      })
+      const slideOption = {
+          slidesPerView: 6,
+          speed: 400,
+        }
+
+        return {
+          slideOption
+        }
     }
   }
 </script>
