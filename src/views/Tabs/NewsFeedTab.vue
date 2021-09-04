00a4f2de8230904a50f60f7ddae4da167fb3bb80
badchita <47920377+badchita@users.noreply.github.com>
2021-09-04 14:51:16 +0800

    NewsFeedTab.vue
    -posts styles

--- src/views/Tabs/NewsFeedTab.vue
+++ src/views/Tabs/NewsFeedTab.vue
@@ -79,50 +79,59 @@
 
       <ion-grid class="posts">
         <ion-row class="post-details">
-          <ion-col>
+          <ion-col size="1.6">
             <ion-avatar><img src="@/assets/images/profile-picture.jpg" /></ion-avatar>
           </ion-col>
-          <ion-col>
-            <ion-label>Vr Bachita</ion-label>
-          </ion-col>
-          <ion-col>
-            <ion-label>
-              <ion-icon name="ellipsis-horizontal" />
-            </ion-label>
-          </ion-col>
-          <ion-col>
-            <ion-label>13m</ion-label>
-          </ion-col>
-          <ion-col>
-            <ion-label>
+          <ion-col size="9.4" class="post-name">
+            <ion-label class="name">Vr Bachita</ion-label><br />
+            <ion-label class="time">13m
               <ion-icon name="people" />
             </ion-label>
           </ion-col>
+          <ion-col size="1">
+            <ion-label>
+              <ion-icon size="small" slot="icon-only" name="ellipsis-horizontal" />
+            </ion-label>
+          </ion-col>
         </ion-row>
+        <!-- <ion-row class="description">
+          <ion-col>
+          <p>Bike Bike</p>
+          </ion-col>
+        </ion-row> -->
         <ion-row class="post-image">
           <img src="@/assets/images/profile-picture.jpg" />
         </ion-row>
         <ion-row class="reacts-comments">
-          <ion-col>
-            <ion-icon name="thumbs-up-sharp" />
+          <ion-col size=".4">
+            <ion-icon color="primary" name="thumbs-up-sharp" />
           </ion-col>
-          <ion-col>
-            <ion-icon name="heart-circle-sharp" />
+          <ion-col size=".6">
+            <ion-icon color="danger" name="heart-circle-sharp" />
+          </ion-col>
+          <ion-col size="7.8">
+            Maria Pico and 33 others
           </ion-col>
           <ion-col>3 Comments</ion-col>
         </ion-row>
         <ion-row class="react-comment-share">
           <ion-col>
-            <ion-icon name="thumbs-up-outline" />
-            Like
+            <div class="like">
+              <ion-icon name="thumbs-up-outline" />
+              Like
+            </div>
           </ion-col>
           <ion-col>
-            <ion-icon name="chatbox-outline" />
-            Comment
+            <div class="comment">
+              <ion-icon name="chatbox-outline" />
+              Comment
+            </div>
           </ion-col>
           <ion-col>
-            <ion-icon name="arrow-redo-outline" />
-            Share
+            <div class="share">
+              <ion-icon name="arrow-redo-outline" />
+              Share
+            </div>
           </ion-col>
         </ion-row>
       </ion-grid>
