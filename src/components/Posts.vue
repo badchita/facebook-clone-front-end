eee26838c7d4dee74ea31289500c326f652fe9cd
badchita <47920377+badchita@users.noreply.github.com>
2021-09-04 16:54:11 +0800

    Post.vue
    -Post component
    -post scss

---
+++ src/components/Posts.vue
@@ -0,0 +1,89 @@
+<template>
+    <ion-grid class="posts" v-for="(item,i) in 10" :key="i">
+        <ion-row class="post-details">
+            <ion-col size="1.8">
+                <ion-avatar><img src="@/assets/images/profile-picture.jpg" /></ion-avatar>
+            </ion-col>
+            <ion-col size="9.2" class="post-name">
+                <ion-label class="name">Vr Bachita</ion-label><br />
+                <ion-label class="time">13m
+                    <ion-icon name="people" />
+                </ion-label>
+            </ion-col>
+            <ion-col size="1">
+                <ion-label>
+                    <ion-icon size="small" slot="icon-only" name="ellipsis-horizontal" />
+                </ion-label>
+            </ion-col>
+        </ion-row>
+        <!-- <ion-row class="description">
+          <ion-col>
+          <p>Bike Bike</p>
+          </ion-col>
+        </ion-row> -->
+        <ion-row class="post-image">
+            <img src="@/assets/images/profile-picture.jpg" />
+        </ion-row>
+        <ion-row class="reacts-comments">
+            <ion-col size=".4">
+                <ion-icon color="primary" src="thumbs-up-sharp" />
+            </ion-col>
+            <ion-col size=".6">
+                <ion-icon color="danger" name="heart-circle-sharp" />
+            </ion-col>
+            <ion-col size="7.8">
+                Maria Pico and 33 others
+            </ion-col>
+            <ion-col>3 Comments</ion-col>
+        </ion-row>
+        <ion-row class="react-comment-share">
+            <ion-col>
+                <div class="like">
+                    <ion-icon name="thumbs-up-outline" />
+                    Like
+                </div>
+            </ion-col>
+            <ion-col>
+                <div class="comment">
+                    <ion-icon name="chatbox-outline" />
+                    Comment
+                </div>
+            </ion-col>
+            <ion-col>
+                <div class="share">
+                    <ion-icon name="arrow-redo-outline" />
+                    Share
+                </div>
+            </ion-col>
+        </ion-row>
+    </ion-grid>
+</template>
+
+<script>
+    import {
+        IonGrid,
+        IonRow,
+        IonCol,
+        IonAvatar,
+        IonIcon,
+        IonLabel,
+    } from '@ionic/vue';
+    export default {
+        components: {
+            IonGrid,
+            IonRow,
+            IonCol,
+            IonAvatar,
+            IonIcon,
+            IonLabel,
+        },
+        setup() {
+            return {}
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import '@/assets/css/variables.scss';
+    @import '@/assets/css/posts.scss';
+</style>
--- src/views/Tabs/NewsFeedTab.vue
+++ src/views/Tabs/NewsFeedTab.vue
@@ -1,10 +1,10 @@
 <template>
   <ion-page>
-    <ion-header>
+    <ion-header v-show="scrolling === false">
       <MainHeader :title="'facebook'" />
     </ion-header>
 
-    <ion-content>
+    <ion-content :scroll-events="true" @ionScroll="onScroll($event)">
       <ion-grid class="create-status-grid">
         <ion-row>
           <ion-col size="1.2">
@@ -77,64 +77,7 @@
         </ion-row>
       </ion-grid>
 
-      <ion-grid class="posts">
-        <ion-row class="post-details">
-          <ion-col size="1.6">
-            <ion-avatar><img src="@/assets/images/profile-picture.jpg" /></ion-avatar>
-          </ion-col>
-          <ion-col size="9.4" class="post-name">
-            <ion-label class="name">Vr Bachita</ion-label><br />
-            <ion-label class="time">13m
-              <ion-icon name="people" />
-            </ion-label>
-          </ion-col>
-          <ion-col size="1">
-            <ion-label>
-              <ion-icon size="small" slot="icon-only" name="ellipsis-horizontal" />
-            </ion-label>
-          </ion-col>
-        </ion-row>
-        <!-- <ion-row class="description">
-          <ion-col>
-          <p>Bike Bike</p>
-          </ion-col>
-        </ion-row> -->
-        <ion-row class="post-image">
-          <img src="@/assets/images/profile-picture.jpg" />
-        </ion-row>
-        <ion-row class="reacts-comments">
-          <ion-col size=".4">
-            <ion-icon color="primary" name="thumbs-up-sharp" />
-          </ion-col>
-          <ion-col size=".6">
-            <ion-icon color="danger" name="heart-circle-sharp" />
-          </ion-col>
-          <ion-col size="7.8">
-            Maria Pico and 33 others
-          </ion-col>
-          <ion-col>3 Comments</ion-col>
-        </ion-row>
-        <ion-row class="react-comment-share">
-          <ion-col>
-            <div class="like">
-              <ion-icon name="thumbs-up-outline" />
-              Like
-            </div>
-          </ion-col>
-          <ion-col>
-            <div class="comment">
-              <ion-icon name="chatbox-outline" />
-              Comment
-            </div>
-          </ion-col>
-          <ion-col>
-            <div class="share">
-              <ion-icon name="arrow-redo-outline" />
-              Share
-            </div>
-          </ion-col>
-        </ion-row>
-      </ion-grid>
+      <Posts />
     </ion-content>
   </ion-page>
 </template>
@@ -159,8 +102,9 @@
   } from '@ionic/vue';
 
   import MainHeader from '@/components/MainHeader.vue'
+  import Posts from '@/components/Posts.vue'
   import {
-    onMounted
+    onMounted, ref
   } from '@vue/runtime-core';
 
   export default {
@@ -182,7 +126,8 @@
       IonCard,
       IonCardContent,
 
-      MainHeader
+      MainHeader,
+      Posts
     },
     setup() {
       onMounted(() => {
@@ -198,9 +143,17 @@
         speed: 400,
       }
 
+      let scrolling = ref(false)
+
+      function onScroll(ev) {
+        ev.detail.scrollTop > 0 ? scrolling.value = true : scrolling.value = false
+      }
+
       return {
         slideOption,
-        slideOptionStories
+        slideOptionStories,
+        onScroll,
+        scrolling
       }
     }
   }
@@ -209,5 +162,4 @@
 <style lang="scss" scoped>
   @import '@/assets/css/variables.scss';
   @import '@/assets/css/news-feed-tab.scss';
-  @import '@/assets/css/global.scss';
 </style>
